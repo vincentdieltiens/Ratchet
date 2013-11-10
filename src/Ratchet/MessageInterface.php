623f03fffbcfdb257c10753ea46aaf4769401dd3
Vincent Dieltiens <and1hotsauce@gmail.com>
2013-11-10 15:42:46 +0100

    Update the MessageInterface to add onBinaryMessage

--- src/Ratchet/MessageInterface.php
+++ src/Ratchet/MessageInterface.php
@@ -3,10 +3,18 @@ namespace Ratchet;
 
 interface MessageInterface {
     /**
-     * Triggered when a client sends data through the socket
+     * Triggered when a client sends non binary data through the socket
      * @param  \Ratchet\ConnectionInterface $from The socket/connection that sent the message to your application
      * @param  string                       $msg  The message received
      * @throws \Exception
      */
     function onMessage(ConnectionInterface $from, $msg);
+
+    /**
+     * Triggered when a client sends binary data through the socket
+     * @param  \Ratchet\ConnectionInterface $from The socket/connection that sent the message to your application
+     * @param  string                       $msg  The message received
+     * @throws \Exception
+     */
+    function onBinaryMessage(ConnectionInterface $from, $msg);
 }
